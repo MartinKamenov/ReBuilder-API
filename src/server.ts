124567d2d768968cfd9e436f42cc786c5b548cb3
MartinKamenov <martinkamenov10@gmail.com>
2019-06-12 21:25:48 +0300

    Import session and parser packages

--- src/server.ts
+++ src/server.ts
@@ -3,10 +3,32 @@ import SetupConfiguration from "./setup/SetupConfiguration";
 const express = require('express');
 const cors = require('cors');
 
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+const flash = require('connect-flash');
+const bodyParser = require('body-parser');
+
+const sessionSecret = process.env.sessionSecret || 
+    require('../../credentials/credentialManager').sessionSecret;
+
 const start = (setupConfiguration: SetupConfiguration) => {
     const app = express();
 
     app.use(cors());
+
+    app.use(cookieParser('secret'));
+    app.use(session({
+        secret: sessionSecret,
+        resave: false,
+        saveUninitialized: true,
+        cookie: {
+            maxAge: 600000000
+        },
+    }));
+    app.use(flash());
+    app.use(bodyParser.urlencoded({ extended: true }));
+
+    app.use(bodyParser.json());
 
     app.listen(setupConfiguration.port, setupConfiguration.listenCallback);
 };
