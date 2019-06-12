57dd315308a38ed0f4750652f6743626123fd01d
MartinKamenov <martinkamenov10@gmail.com>
2019-06-12 20:15:42 +0300

    Extract route interface, install passport and cors packages

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
 import SetupConfiguration from "./setup/SetupConfiguration";
 
 const express = require('express');
+const cors = require('cors');
 
 const start = (setupConfiguration: SetupConfiguration) => {
     const app = express();
+
+    app.use(cors());
 
     app.listen(setupConfiguration.port, setupConfiguration.listenCallback);
 };
