1aa84352a7bcffdc0e0e1972f4543c925e4ca6bc
MartinKamenov <martinkamenov10@gmail.com>
2019-06-17 19:26:48 +0300

    Create user model which implements user

--- src/models/repositories/ProjectRepository.ts
+++ src/models/repositories/ProjectRepository.ts
@@ -1,21 +1,21 @@
-import Project from "../Project";
+import Project from "../contracts/Project";
 import Database from "../../database/Database";
 
 export default class ProjectRepository {
     constructor(private database: Database, private collectionName: String) {}
-    getAllProjects() {
+    getAllProjects(): Promise<Project[]> {
         return this.database.showAll(this.collectionName);
     }
 
-    addProject(user: Project) {
+    addProject(user: Project): Promise<Project[]> {
         return this.database.insert(this.collectionName, user);
     }
 
-    findProjectById(id: String) {
+    findProjectById(id: String): Promise<Project[]> {
         return this.database.find(this.collectionName, { id });
     }
 
-    updateProject(id: String, newProject: Project) {
+    updateProject(id: String, newProject: Project): Promise<Project[]> {
         return this.database.update(this.collectionName, { id }, newProject);
     }
 };
