ff95bcccff20950bffd6fefed43ef94d2d6ae837
MartinKamenov <martinkamenov10@gmail.com>
2019-06-19 10:24:19 +0300

    Fix connector

--- src/database/connector.ts
+++ src/database/connector.ts
@@ -1,4 +1,4 @@
-import Database from "./database";
+import Database from "./Database";
 
 const username = process.env.dbUser || 
     require('../../../credentials/credentialManager').rebuilderUsername;
