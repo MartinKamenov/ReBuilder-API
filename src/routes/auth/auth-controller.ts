7f75d68e2aba90a5b356529d4e45aeb4efb157e8
MartinKamenov <martinkamenov10@gmail.com>
2019-07-05 19:53:14 +0300

    Remove old getUser method

--- src/routes/auth/auth-controller.ts
+++ src/routes/auth/auth-controller.ts
@@ -94,11 +94,7 @@ const controller = {
     logout: (requestObject: LogoutRequest) => {
         requestObject.logout();
         return constants.SUCCESSFULL_LOGOUT_MESSAGE;
-    },
-
-    getUser: (requestObject: AuthenticatedRequest) => {
-        return requestObject.user;
-    },
+    }
 };
 
 export default controller;
