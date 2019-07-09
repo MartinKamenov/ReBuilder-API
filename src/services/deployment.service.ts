b5d5c8122a3ecca5110ff81a02315a0752c43f83
MartinKamenov <martinkamenov10@gmail.com>
2019-07-09 19:28:18 +0300

    Auto generated commit

--- src/services/deployment.service.ts
+++ src/services/deployment.service.ts
@@ -2,6 +2,9 @@ import projectSavingDeploymentService from './projectSavingDeployment.service';
 import Project from '../models/contracts/Project';
 const exec = require('child-process-promise').exec;
 
+function sleep(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+}
 const deploymentService = {
     deployProject: async (project: Project): Promise<string> => {
         await projectSavingDeploymentService
@@ -9,18 +12,18 @@ const deploymentService = {
         const path = './deployments/' + project.id;
         try {
             const name = project.name.toLowerCase().trim().replace(/\s/g, '-');
-            console.log(name);
-            // const herokuGitUrl = 'https://git.heroku.com/' + name + '.git';
-            const commands = [`cd ${path} && git init && heroku create ${name}`,
-                `cd ${path} &&` +
+            const herokuGitUrl = 'https://git.heroku.com/' + name + '.git';
+            const commands = [
+                `git init`,
+                `heroku create ${name}`,
+                `git remote add origin ${herokuGitUrl}`,
                 `git add . && git commit -m "Auto generated commit"`,
-                `cd ${path} && git push heroku master`];
-            await commands.forEach(async (command) => {
-                await setTimeout(async () => {
-                    const { stdout } = await exec(command);
-                    // tslint:disable-next-line:no-console
-                    console.log(stdout);
-                }, 2000);
+                `git push -u origin master`];
+            commands.forEach(async (command) => {
+                await exec(command);
+                // tslint:disable-next-line:no-console
+                console.log('executed ' + command);
+                await sleep(3000);
             });
 
             return `https://${name}.herokuapp.com`;
