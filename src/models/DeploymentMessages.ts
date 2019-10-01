534ea6a473095c2512336e886005060eba80bccd
MartinKamenov <martinkamenov10@gmail.com>
2019-10-01 18:59:33 +0300

    Update deployment messages

---
+++ src/models/DeploymentMessages.ts
@@ -0,0 +1,9 @@
+const deploymentMessages = {
+    INIT_PROJECT: 'Deployment started',
+    HEROKU_CREATE: 'Project was created',
+    GIT_REMOTE: 'Changes were made',
+    GIT_ADD: 'Changes were saved',
+    GIT_PUSH: 'Updating project'
+};
+
+export default deploymentMessages;
